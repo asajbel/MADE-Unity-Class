44d4dc4536b815a90e60f5c024af122c5516ba6c
asajbel <serdan66@gmail.com>
2014-10-10 22:02:44 -0700

    Added Second Level and Scene Change

---
+++ Class2D/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+	private bool raised = false; 
+	private Animator anim;
+
+	void Awake () {
+		anim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool("Raised", raised);
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.transform.tag == "Player") {
+			raised = true;
+		}
+	}
+
+	void NextLevel(string level) {
+		Application.LoadLevel(level); 
+	}
+}
